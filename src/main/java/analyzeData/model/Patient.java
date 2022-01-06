6a19e99d2bfcdf91e864d96993b5006dd4b5a51e
LisaCarranque <lisacarranquerios@protonmail.com>
2022-01-06 18:21:56 +0100

    enhance validation for microservice analyzeData model

--- src/main/java/analyzeData/model/Patient.java
+++ src/main/java/analyzeData/model/Patient.java
@@ -3,6 +3,7 @@ package analyzeData.model;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -21,17 +22,21 @@ public class Patient {
     Integer id;
     @NonNull
     @NotEmpty
+    @Size(min=1, max=40)
     String firstname;
     @NonNull
     @NotEmpty
+    @Size(min=1, max=40)
     String lastname;
     @NonNull
     Gender gender;
     @NotEmpty
     @NonNull
+    @Size(min=1, max=40)
     String address;
     @NonNull
     @NotEmpty
+    @Size(min=1, max=40)
     String phone;
     @NonNull
     LocalDate birthdate;
